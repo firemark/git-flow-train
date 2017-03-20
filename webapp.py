db91d05aa60e4a4be3309410e5ad354d6c89c9e4
Firemark <marpiechula@gmail.com>
2017-03-20 13:45:15 +0100

    Printowanie danych, zmiana index.html

--- webapp.py
+++ webapp.py
@@ -1,10 +1,18 @@
 from flask import Flask
 from flask import render_template
+from collections import namedtuple
+
 app = Flask(__name__)
+
+data = namedtuple('Row', 'country local_price dollar_ex dollar_price dollar_ppp dollar_valuation')
+
+def load_data():
+    return [data('Ala', 32, 21, 312, 32, 23), 
+            data('Ahoy', 214, 2, 13, 2, 1)]
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return render_template('index.html', data=load_data())
 
 
 @app.route('/hello/<name>')
