19a72797f6a469185b0b22a228bbc02ffd05d078
Firemark <marpiechula@gmail.com>
2017-03-20 13:45:15 +0100

    change index.html

--- webapp.py
+++ webapp.py
@@ -3,7 +3,16 @@ from flask import render_template
 app = Flask(__name__)
 
 
+def load_data():
+    return [{'country': 'Ala', 'local_price': 21, 'dollar_ex':2, 'dollar_price': 11, 'dollar_ppp': 4, 'dollar_valuation': 22},
+            {'country': 'AlASa', 'local_price': 21, 'dollar_ex':2, 'dollar_price': 11, 'dollar_ppp': 4, 'dollar_valuation': 22}]
 @app.route('/')
-def hello(name=None):
-    return render_template('./index.html', name=name)
-
+def hello_world():
+    data = load_data()
+    headers = data[0]
+    print(headers)
+    body = data[1:]
+    print list(body)
+    # print(body)
+    
+    return render_template('index.html', data=data, headers=headers)
